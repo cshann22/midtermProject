3bf1ac52efba0783c66239cf8eb609b9006bd90d
chwoodv <148597237+chwoodv@users.noreply.github.com>
2024-03-03 11:23:56 -0600

    Changes

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ function appendTable(data) {
     for (let i = 0; i < data.elements.length; i++) {
         console.log(data.elements[i]);
         let tr = document.createElement("tr");
-        mainContainer.appendChild(tr);
-        for (let j = i; data.elements[j].group < 18; j++) {
-            // let td = document.createElement("td");
-            tr.innerHTML = tr.innerHTML + `<td class="${data.elements[j].category}">${data.elements[j].symbol}</td>
-            `;
-            // tr.appendChild(td);
+        tr.innerHTML = tr.innerHTML + `<td class="${data.elements[i].category}">${data.elements[i].symbol}</td>
+        `;
+        // for (let j = i; data.elements[j].group != 1; j++) {
+        //     tr.innerHTML = tr.innerHTML + `<td class="${data.elements[j].category}">${data.elements[j].symbol}</td>
+        //     `;
+        //     i++;
+        // }
+        do {
             i++;
-        }
+            tr.innerHTML = tr.innerHTML + `<td class="${data.elements[i].category}">${data.elements[i].symbol}</td>
+            `;
+        } while (data.elements[i].group < 18);
+
         console.log(tr.innerHTML);
+        mainContainer.appendChild(tr);
+
     }
     console.log(mainContainer.innerHTML);
 
@@ -23,9 +30,6 @@ fetch('./data.json')
         return response.json();
     })
     .then(function (data) {
-        // console.log(data);
-        // console.log(data.firstName);
-        // console.log(data["firstName"]);
         appendTable(data);
     })
     .catch(function (err) {
